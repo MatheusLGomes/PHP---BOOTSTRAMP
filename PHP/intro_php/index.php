9a91958449fc9d85f636d406d9eedc258a42c438
MatheusLGomes <111156565+MatheusLGomes@users.noreply.github.com>
2023-05-09 23:35:50 -0300

    Add files via upload

---
+++ PHP/intro_php/index.php
@@ -0,0 +1,19 @@
+<?php
+echo "Olá mundo! <br>";
+print  "Olá mundo! <br>";
+
+echo 1;
+echo "<br>";
+echo 1 + 1;
+echo"<br>";
+echo -1;
+echo "<br>";
+echo 1-2;
+echo "<br>";
+
+if(is_int(1)){
+    echo "É inteiro <br>";
+
+}else{
+    echo "Não é inteiro<br>";
+}
